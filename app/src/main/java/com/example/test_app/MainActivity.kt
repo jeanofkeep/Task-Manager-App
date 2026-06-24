609b35bce4b637e6341617a0a8451ac7c1989058
jeanofkeep <vadimmoshkov56@gmail.com>
2026-06-25 02:01:03 +0300

    add settings screen

--- app/src/main/java/com/example/test_app/MainActivity.kt
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -4,6 +4,7 @@ import com.example.test_app.screens.ShoppingScreen
 import com.example.test_app.screens.ProjectsScreen
 import com.example.test_app.screens.TasksScreen
 import com.example.test_app.screens.ProjectDetailScreen
+import com.example.test_app.screens.SettingsScreen
 
 
 
@@ -12,12 +13,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import com.composables.icons.lucide.Lucide
 import com.composables.icons.lucide.Check
 import com.composables.icons.lucide.FolderOpen
 import com.composables.icons.lucide.ShoppingCart
+import com.composables.icons.lucide.Settings
+
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -37,6 +41,7 @@ sealed class Screen(val route: String, val title: String) {
     object Tasks : Screen("tasks", "Задачи")
     object Projects : Screen("projects", "Проекты")
     object Shopping : Screen("shopping", "Покупки")
+    object Settings : Screen("settings", "Настройки")
 }
 
 class MainActivity : ComponentActivity() {
@@ -57,44 +62,55 @@ fun MainScreen() {
     val navController = rememberNavController()
     val items = listOf(Screen.Tasks, Screen.Projects, Screen.Shopping)
     val icons = listOf(Lucide.Check, Lucide.FolderOpen, Lucide.ShoppingCart)
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
     //val icons = listOf(Icons.Filled.CheckBox, Icons.Filled.Folder, Icons.Filled.ShoppingCart)
+    val title = when (currentRoute) {
+        Screen.Settings.route -> "Настройки"
+        else -> "Главная"
+    }
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("Главная") })
+            TopAppBar(title = { Text(title) },
+            actions = {
+                IconButton(onClick = { navController.navigate(Screen.Settings.route)}) {
+                    Icon(Lucide.Settings, contentDescription = "Настройки")
+                }
+            })
         },
-        bottomBar = {
-            NavigationBar {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentRoute = navBackStackEntry?.destination?.route
 
-                items.forEachIndexed { index, screen ->
-                    NavigationBarItem(
-                        icon = { Icon(icons[index], contentDescription = screen.title) },
-                        label = { Text(screen.title) },
-                        selected = currentRoute == screen.route,
-                        colors = NavigationBarItemDefaults.colors(
-                            selectedTextColor = MaterialTheme.colorScheme.onSurface,
-                            unselectedTextColor = MaterialTheme.colorScheme.onSurface,
-                            selectedIconColor = MaterialTheme.colorScheme.primary,
-                            unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                            indicatorColor = MaterialTheme.colorScheme.secondaryContainer
-                        ),
-                        onClick = {
-                            if (currentRoute == screen.route) {
-                                // Если мы уже на этом экране, сбрасываем стек до корня этого экрана
-                                navController.popBackStack(screen.route, false)
-                            } else {
-                                navController.navigate(screen.route) {
-                                    popUpTo(navController.graph.findStartDestination().id) {
-                                        saveState = true
+        bottomBar = {
+
+            if (currentRoute != Screen.Settings.route) {
+                NavigationBar {
+                    items.forEachIndexed { index, screen ->
+                        NavigationBarItem(
+                            icon = { Icon(icons[index], contentDescription = screen.title) },
+                            label = { Text(screen.title) },
+                            selected = currentRoute == screen.route,
+                            colors = NavigationBarItemDefaults.colors(
+                                selectedTextColor = MaterialTheme.colorScheme.onSurface,
+                                unselectedTextColor = MaterialTheme.colorScheme.onSurface,
+                                selectedIconColor = MaterialTheme.colorScheme.primary,
+                                unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                indicatorColor = MaterialTheme.colorScheme.secondaryContainer
+                            ),
+                            onClick = {
+                                if (currentRoute == screen.route) {
+                                    navController.popBackStack(screen.route, false)
+                                } else {
+                                    navController.navigate(screen.route) {
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            saveState = true
+                                        }
+                                        launchSingleTop = true
+                                        restoreState = true
                                     }
-                                    launchSingleTop = true
-                                    restoreState = true
                                 }
                             }
-                        }
-                    )
+                        )
+                    }
                 }
             }
         }
@@ -116,6 +132,8 @@ fun MainScreen() {
                 val projectId = backStackEntry.arguments?.getLong("projectId") ?: 0L
                 ProjectDetailScreen(projectId = projectId)
             }
+
+            composable(Screen.Settings.route) { SettingsScreen() }
         }
     }
 }
